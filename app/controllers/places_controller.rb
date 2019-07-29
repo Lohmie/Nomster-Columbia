c48e4a820e96b60a5242fd072a853106948491d9
Mariam Habib <mariamjhabib@gmail.com>
2019-07-29 15:04:27 +0000

    Lesson 19

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,24 @@
 class PlacesController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
     def index
-        @places = Place.all
+        page = params[:page]
+        defined?(page) ? page=page : page = 0 #assigned the parameter to a local variable and we just want to check it has a value, if this is true, then do what is after the question mark. if false do what's after the colon
+        @places = Place.order("name").page(params[:page]).per_page(10)
+    end
+
+    def new
+       # @place = Place.new
+    end
+
+    def create
+        #Place.create(place_params)
+        current_user.places.create(place_params)
+        redirect_to root_path
+    end
+    
+    private
+    
+    def place_params
+        params.require(:place).permit(:name, :description, :address)
     end
 end
